75e85b07a14e946dd0cc81fca95622802600310b
Carlos Valiente <carlos@pepelabs.net>
2015-10-24 11:07:11 +0100

    Chapter 3

--- 03-const-the-magnificent.scm
+++ 03-const-the-magnificent.scm
@@ -64,3 +64,58 @@
 	(cons new (cdr lat)))
        (else (cons (car lat)
 		   (subst new old (cdr lat)))))))))
+
+(define subst2
+  (lambda (new old1 old2 lat)
+    (cond
+     ((null? lat) '())
+     (else
+      (cond
+       ((or (eq? (car lat) old1) (eq? (car lat) old2))
+	(cons new (cdr lat)))
+       (else (cons (car lat)
+		   (subst2 new old1 old2 (cdr lat)))))))))
+
+;; Based on the simplified version of `rember`
+
+(define multirember
+  (lambda (a lat)
+    (cond
+     ((null? lat) '())
+     ((eq? a (car lat)) (multirember a (cdr lat)))
+     (else (cons (car lat) (multirember a (cdr lat)))))))
+
+(define multiinsertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     (else
+      (cond
+       ((eq? (car lat) old)
+	(cons old
+	      (cons new (multiinsertR new old (cdr lat)))))
+       (else (cons (car lat)
+		   (multiinsertR new old (cdr lat)))))))))
+
+(define multiinsertL
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     (else
+      (cond
+       ((eq? (car lat) old)
+	(cons new
+	      (cons old (multiinsertL new old (cdr lat)))))
+       (else (cons (car lat)
+		   (multiinsertL new old (cdr lat)))))))))
+
+(define multisubst
+  (lambda (new old lat)
+    (cond
+     ((null? lat) '())
+     (else
+      (cond
+       ((eq? (car lat) old)
+	(cons new (multisubst new old (cdr lat))))
+       (else (cons (car lat)
+		   (multisubst new old (cdr lat)))))))))
