00d67aac42923819eb220de095062b9972b09a5f
Carlos Valiente <carlos@pepelabs.net>
2015-12-13 15:47:52 +0000

    Chapter 9 (unfinished)

--- 09-and-again-and-again.scm
+++ 09-and-again-and-again.scm
@@ -1,3 +1,53 @@
+;; From previous chapters
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define plus
+  (lambda (n m)
+    (cond
+     ((zero? m) n)
+     (else
+      (add1 (plus n (sub1 m)))))))
+
+(define times
+  (lambda (n m)
+    (cond
+     ((zero? m) 0)
+     (else
+      (plus n (times n (sub1 m)))))))
+
+(define (atom? x)
+  (and (not (pair? x)) (not (null? x))))
+
+(define build
+  (lambda (s1 s2)
+    (cons s1 (cons s2 '()))))
+
+(define first
+  (lambda (p)
+    (car p)))
+
+(define second
+  (lambda (p)
+    (car (cdr p))))
+
+(define a-pair?
+  (lambda (x)
+    (cond
+     ((atom? x) #f)
+     ((null? x) #f)
+     ((null? (cdr x)) #f)
+     ((null? (cdr (cdr x))) #t)
+     (else #f))))
+
+;; Off we go!
+
 (define looking
   (lambda (a lat)
     (keep-looking a (pick 1 lat) lat)))
@@ -35,3 +85,71 @@
 ;; Functions like `keep-looking` are called _partial functions_, because they
 ;; may not terminate (unlike all other functions we've seen so far, which are
 ;; called _total functions_.
+
+(define shift
+  (lambda (pair)
+    (build
+     (first (first pair))
+     (build
+      (second (first pair))
+      (second pair)))))
+
+;; `align`, defined here, is potentially partial:
+
+(define align
+  (lambda (pora)
+    (cond
+     ((atom? pora) pora)
+     ((a-pair? (first pora))
+      (align (shift pora)))
+     (else
+      (build (first pora)
+	     (align (second pora)))))))
+
+;; According to the text, it is potentially partial because it violates the
+;; Seventh Commandment: `(align (shift pora))` creates an argument for
+;; the recursive call to `align` which is not part of the original argument.
+;;
+;; However, if we could show that the arguments to the recursove calls of
+;; `align` get *simpler* (that is, closer to being an atom), then we could be
+;; certin that `align` is total.
+;;
+;; The text now goes on to build ways of quantifying how much simpler `pora`
+;; gets. The first atempt is `length*`, which counts the number of atoms in
+;; `pora`
+
+(define length*
+  (lambda (pora)
+    (cond
+     ((atom? pora) 1)
+     (else
+      (plus (length* (first pora))
+	    (length* (second pora)))))))
+
+;; This however, does not pay enough attention to the first component of `pora`
+;; when it is a pair:
+;;
+;;     scheme@(guile-user)> (length* '((a b) c))
+;;     $7 = 3
+;;     scheme@(guile-user)> (length* '(a (b c)))
+;;     $8 = 3
+;;
+;; In order to mitigate that, `weight*` is presented:
+
+(define weight*
+  (lambda (pora)
+    (cond
+     ((atom? pora) 1)
+     (else
+      (plus (times (weight* (first pora)) 2)
+	    (weight* (second pora)))))))
+
+;; `weight*` shows better whether a `pora` gets simpler:
+;;
+;;     scheme@(guile-user)> (weight* '((a b) c))
+;;     $9 = 7
+;;     scheme@(guile-user)> (weight* '(a (b c)))
+;;     $10 = 5
+;;
+;; The text concludes that `align` is in fact total, because the values of
+;; `weight*` for the recursive calls to itself get smaller.
